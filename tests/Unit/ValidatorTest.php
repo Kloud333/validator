fb2ee433080c44b30c4a3e477e95b8d8e342c673
Volodymyr Klekot <klekotVR@gmail.com>
2018-04-19 23:51:26 +0300

    added validator test

--- tests/Unit/ValidatorTest.php
+++ tests/Unit/ValidatorTest.php
@@ -2,10 +2,15 @@
 
 namespace tests\Unit;
 
+use app\src\Validator;
+
 class ValidatorTest extends Base
 {
     public function testValidation()
     {
+        $v = new Validator();
+        $a = $v->getRule('IsNumeric')->validate('aaa');
 
+        var_dump($a);
     }
 }
