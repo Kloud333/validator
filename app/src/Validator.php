fa515a0d1caca94fae23901f6bdea2b727c88eae
Volodymyr Klekot <klekotVR@gmail.com>
2018-04-19 23:51:01 +0300

    updated validator

--- app/src/Validator.php
+++ app/src/Validator.php
@@ -2,7 +2,30 @@
 
 namespace app\src;
 
+
 class Validator
 {
+    public $rule;
 
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function getRule($name)
+    {
+        $a = 'app\src\Rules\\'.$name;
+        $this->rule = new $a;
+
+        return $this;
+
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function validate($data)
+    {
+        return $this->rule->validate($data);
+    }
 }
