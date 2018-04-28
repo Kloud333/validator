6f0a3f8c98fe46d582a49f7e6f945b64efa7fdb7
Volodymyr Klekot <klekotVR@gmail.com>
2018-04-28 05:06:49 +0300

    updated rules

--- app/src/Rules/AbstractRule.php
+++ app/src/Rules/AbstractRule.php
@@ -7,9 +7,9 @@ abstract class AbstractRule
     /**
      * @return object
      */
-    protected function createException($message = null)
+    protected function createException($errorMessage = null)
     {
         $exceptionObject = str_replace('\\Rules\\', '\\Exceptions\\', get_called_class()) . 'Exception';
-        return new $exceptionObject($message);
+        return new $exceptionObject($errorMessage);
     }
 }
